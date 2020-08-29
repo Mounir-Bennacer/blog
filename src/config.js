e10be25c1c41462a4941bf2b13867e6e2b9414d5
Mounir Bennacer <benphaoris@gmail.com>
2020-08-29 12:37:36 +0100

    first commit

---
+++ src/config.js
@@ -0,0 +1,26 @@
+export const config = {
+  // Your Github Converted Token To Know How To Get Your Token Look at Readme.md
+  githubConvertedToken: "NDJkYTNmNmZlYmE5OGU2NTFmOWI3MTg5MTJiN2RkODNhYjZjMzk2YQ==",
+
+  // Your Github UserName
+  githubUserName: "Mounir-Bennacer",
+
+  // Your Github Repo Name Where You Have your issues as Blog
+  githubRepo: "react-blog-github",
+
+  // Your Personal Blog Title
+  title : "Mounir Bennacer" ,
+
+  // Your Personal Blog Subtitle
+  subtitle : "Software Developer",
+
+  // Header customization
+  header: {
+    backgroundColor: '#f1f6f8', // can be a CSS gradient
+    backgroundColorDark: '#4C566A',
+    titleColor: '#ff5252',
+    titleColorDark: '#16a085',
+    subtitleColor: '#37474f',
+    subtitleColorDark: '#D8DEE9',
+  },
+};
